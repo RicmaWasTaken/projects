4339769cbf1f9a5f1e9a52fc89da9e4d8b3cd127
RicmaWasTaken <114833214+RicmaWasTaken@users.noreply.github.com>
2024-02-09 16:51:00 +0100

    Delete ricma-fr-old directory

--- ricma-fr-old/footer.php
+++
@@ -1,9 +0,0 @@
-<?php
-    echo '
-        <!--début footer-->
-        <footer>
-            <p>© Ricardo Marques 2023, Tous droits réservés.</p>
-        </footer>
-        <!--fin footer-->
-    '
-?>
