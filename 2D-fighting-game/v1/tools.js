daec8e9c8d5bc16c3ec36f156bdd350820cfe47b
RicmaWasTaken <114833214+RicmaWasTaken@users.noreply.github.com>
2024-03-06 21:24:50 +0100

    Added full v1 content

---
+++ 2D-fighting-game/v1/tools.js
@@ -0,0 +1,15 @@
+function hide(element){
+    element.style.display = "none";
+}
+function show(element){
+    element.style.display = "block";
+}
+function showFlex(element){
+    element.style.display = "flex";
+}
+function colorBackground(element, colorCode){
+    element.style.backgroundColor = colorCode;
+}//NEEDS ""
+function colorText(element, colorCode){
+    element.style.color = colorCode;
+}
